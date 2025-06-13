be76889c9b39f1c4db15061cd6ebe763fe43892c
AbsurdGem <mormoo5363@students.ecpi.edu>
2025-06-13 04:10:53 -0400

    Update calculations.c

--- calculations.c
+++ calculations.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
+#include "calculations.h"
 
 void firstCalculation(int a, int b) {
-    // sample implementation
     printf("Sum = %d\n", a + b);
 }
-
 void secondCalculation(int a, int b) {
-    // sample implementation
     printf("Product = %d\n", a * b);
 }
-
